4d549444142c088077909aafae22d2739c08c03d
Sagar Kurandwad <sagar@ai-r.com>
2021-11-12 15:57:58 -0500

    rollout cleanup

--- tf_agents/cogment_verse_tf_agents/reinforce/training_run.py
+++ tf_agents/cogment_verse_tf_agents/reinforce/training_run.py
@@ -65,6 +65,7 @@ def create_training_run(agent_adapter):
         try:
             # Initializing a model
             model_id = f"{run_id}_model"
+            model_version_number = 1
             model_kwargs = MessageToDict(config.model_kwargs, preserving_proto_field_name=True)
             model, _ = await agent_adapter.create_and_publish_initial_version(
                 model_id,
@@ -94,15 +95,15 @@ def create_training_run(agent_adapter):
             all_trials_reward = 0
             start_time = time.time()
 
-            # Create the config for the player agents
-            player_actor_configs = [
+            # Create config for the actor
+            actor_configs = [
                 TrialActor(
-                    name=f"agent_player_{player_idx}",
+                    name=f"reinforce_player_{player_idx}",
                     actor_class="agent",
                     implementation=config.agent_implementation,
                     config=ActorConfig(
                         model_id=model_id,
-                        model_version=np.random.randint(-100, -1),  # TODO this actually won't work anymore
+                        model_version=model_version_number,
                         run_id=run_id,
                         env_type=config.environment_type,
                         env_name=config.environment_name,
@@ -112,12 +113,9 @@ def create_training_run(agent_adapter):
                 )
                 for player_idx in range(config.player_count)
             ]
-            # for self-play, randomly select one player to use latest model version
-            # if there is only one player then it will always use the latest
-            distinguished_actor = np.random.randint(0, config.player_count)
-            player_actor_configs[distinguished_actor].config.model_version = -1
 
-            self_play_trial_configs = [
+            # Create configs for trials
+            trial_configs = [
                 TrialConfig(
                     run_id=run_id,
                     environment_config=EnvConfig(
@@ -130,45 +128,10 @@ def create_training_run(agent_adapter):
                         flatten=config.flatten,
                         framestack=config.framestack,
                     ),
-                    actors=player_actor_configs,
-                    distinguished_actor=distinguished_actor,
+                    actors=actor_configs
                 )
-                for _ in range(config.total_trial_count - config.demonstration_count)
+                for _ in range(config.total_trial_count)
             ]
-
-            demonstration_trial_configs = []
-            if config.demonstration_count > 0:
-                # create the config for the teacher agent
-                teacher_actor_config = TrialActor(
-                    name="web_actor",
-                    actor_class="teacher_agent",
-                    implementation="client",
-                    config=ActorConfig(
-                        run_id=run_id,
-                        env_type=config.environment_type,
-                        env_name=config.environment_name,
-                        num_input=config.num_input,
-                        num_action=config.num_action,
-                    ),
-                )
-                demonstration_trial_configs = [
-                    TrialConfig(
-                        run_id=run_id,
-                        environment_config=EnvConfig(
-                            player_count=config.player_count,
-                            run_id=run_id,
-                            render=True,
-                            render_width=config.render_width,
-                            env_type=config.environment_type,
-                            env_name=config.environment_name,
-                            flatten=config.flatten,
-                            framestack=config.framestack,
-                        ),
-                        actors=[*player_actor_configs, teacher_actor_config],
-                        distinguished_actor=distinguished_actor,
-                    )
-                    for _ in range(config.demonstration_count)
-                ]
 
             async def archive_model(
                 model_archive_schedule,
@@ -205,71 +168,56 @@ def create_training_run(agent_adapter):
                         f"[{run_session.params_name}/{run_id}] {model_id}@v{version_number} {verb} after {run_session.count_steps()} steps ({sizeof_fmt(version_data_size)})"
                     )
 
-            async def run_trials(trial_configs, max_parallel_trials):
-                nonlocal samples_generated
-                nonlocal training_step
-                nonlocal samples_seen
-                nonlocal trials_completed
-                nonlocal all_trials_reward
-                nonlocal start_time
 
-                async for (
-                    step_idx,
-                    step_timestamp,
-                    _trial_id,
-                    _tick_id,
-                    sample,
-                ) in run_session.start_trials_and_wait_for_termination(
-                    trial_configs=trial_configs,
-                    max_parallel_trials=max_parallel_trials,
-                    on_progress=create_progress_logger(run_session.params_name, run_id, config.total_trial_count),
-                ):
-                    if sample.trial_total_reward is not None:
-                        # This is a sample from a end of a trial
+            async for (
+                step_idx,
+                step_timestamp,
+                _trial_id,
+                _tick_id,
+                sample,
+            ) in run_session.start_trials_and_wait_for_termination(
+                trial_configs=trial_configs,
+                max_parallel_trials=config.max_parallel_trials,
+                on_progress=create_progress_logger(run_session.params_name, run_id, config.total_trial_count),
+            ):
+                if sample.trial_total_reward is not None:
+                    # This is a sample from a end of a trial
 
-                        trials_completed += 1
-                        all_trials_reward += sample.trial_total_reward
+                    trials_completed += 1
+                    all_trials_reward += sample.trial_total_reward
 
-                        run_xp_tracker.log_metrics(
-                            step_timestamp,
-                            step_idx,
-                            trial_total_reward=sample.trial_total_reward,
-                            trials_completed=trials_completed,
-                            mean_trial_reward=all_trials_reward / trials_completed,
-                        )
+                    run_xp_tracker.log_metrics(
+                        step_timestamp,
+                        step_idx,
+                        trial_total_reward=sample.trial_total_reward,
+                        trials_completed=trials_completed,
+                        mean_trial_reward=all_trials_reward / trials_completed,
+                    )
 
-                    samples_generated += 1
+                samples_generated += 1
 
-                    with TRAINING_ADD_SAMPLE_TIME.time():
-                        model.consume_training_sample(sample.current_player_sample)
+                with TRAINING_ADD_SAMPLE_TIME.time():
+                    model.consume_training_sample(sample.current_player_sample)
 
-                    TRAINING_REPLAY_BUFFER_SIZE.set(model.get_replay_buffer_size())
+                TRAINING_REPLAY_BUFFER_SIZE.set(model.get_replay_buffer_size())
 
-                    if sample.current_player_sample[-1]:
-                        with TRAINING_LEARN_TIME.time():
-                            info = model.learn()
-                        samples_seen += info["num_samples_seen"]
-                        training_step += 1
+                if sample.current_player_sample[-1]:
+                    with TRAINING_LEARN_TIME.time():
+                        info = model.learn()
+                    samples_seen += info["num_samples_seen"]
+                    training_step += 1
 
-                        await archive_model(
-                            model_archive_schedule,
-                            model_publication_schedule,
-                            step_timestamp,
-                            step_idx,
-                            info
-                        )
+                    await archive_model(
+                        model_archive_schedule,
+                        model_publication_schedule,
+                        step_timestamp,
+                        step_idx,
+                        info
+                    )
 
-                log.info(
-                    f"[{run_session.params_name}/{run_id}] done, {model.get_replay_buffer_size()} samples gathered over {run_session.count_steps()} steps"
-                )
-
-            if demonstration_trial_configs:
-                await run_trials(
-                    demonstration_trial_configs,
-                    max_parallel_trials=config.max_parallel_trials,
-                )
-            if self_play_trial_configs:
-                await run_trials(self_play_trial_configs, max_parallel_trials=config.max_parallel_trials)
+            log.info(
+                f"[{run_session.params_name}/{run_id}] done, {model.get_replay_buffer_size()} samples gathered over {run_session.count_steps()} steps"
+            )
 
             run_xp_tracker.terminate_success()
 
