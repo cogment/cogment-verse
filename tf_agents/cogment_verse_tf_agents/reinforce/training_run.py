453b4826d98973f57692a9dda3667884f0cc00ad
Sagar Kurandwad <sagar@ai-r.com>
2021-11-23 12:56:14 -0500

    apche liceses

--- tf_agents/cogment_verse_tf_agents/reinforce/training_run.py
+++ tf_agents/cogment_verse_tf_agents/reinforce/training_run.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from google.protobuf.json_format import MessageToDict
 
 from data_pb2 import (
