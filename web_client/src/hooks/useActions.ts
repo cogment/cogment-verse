295e1248cb216d293263f3cbd3683989779fd641
Air-sara <sara@ai-r.com>
2022-01-28 15:30:45 -0500

    ConfigT

--- web_client/src/hooks/useActions.ts
+++ web_client/src/hooks/useActions.ts
@@ -36,15 +36,15 @@ export type UseActions = <ObservationT, ActionT extends MessageBase, ConfigT ext
   actorClass: string,
   grpcURL: string
 ) => [
-  event: Event<ObservationT>,
-  JoinTrial: JoinTrial | undefined,
-  sendAction: SendAction<ActionT> | undefined,
-  reset: () => void,
-  trialJoined: boolean,
-  watchTrials: WatchTrials | undefined,
-  trialStateList: TrialStateList | undefined,
-  actorConfig: ConfigT | undefined
-];
+    event: Event<ObservationT>,
+    JoinTrial: JoinTrial | undefined,
+    sendAction: SendAction<ActionT> | undefined,
+    reset: () => void,
+    trialJoined: boolean,
+    watchTrials: WatchTrials | undefined,
+    trialStateList: TrialStateList | undefined,
+    actorConfig: ConfigT | undefined
+  ];
 
 export const useActions: UseActions = <ObservationT, ActionT extends MessageBase, ConfigT extends MessageBase>(
   _cogSettings: CogSettings,
@@ -71,7 +71,7 @@ export const useActions: UseActions = <ObservationT, ActionT extends MessageBase
   const [cogSettings, setCogSettings] = useState(_cogSettings);
   const [watchTrials, setWatchTrials] = useState<WatchTrials>();
 
-  const [actorConfig, setActorConfig] = useState<any>();
+  const [actorConfig, setActorConfig] = useState<ConfigT>();
 
   const reset = useCallback(() => {
     setCogSettings({ ..._cogSettings });
