c9e32dec41ba306a55f4ea48fee8372feb582670
joshair <109359509+joshair@users.noreply.github.com>
2022-08-18 08:57:42 -0400

    Pytorch multiproc fix (#81)
    * Fix config bug
    
    * Fix pytorch multiproccessing bug
    
    * Move torch specific multiprocessing config to torch imports
    
    * Add space to pass license test
    
    Co-authored-by: saikrishnagv_1996 <saikrishnagv1996@gmail.com>

--- main.py
+++ main.py
@@ -14,6 +14,7 @@
 
 import logging
 import os
+import multiprocessing as mp
 
 import hydra
 
@@ -32,4 +33,5 @@ def main(cfg):
 
 
 if __name__ == "__main__":
+    mp.set_start_method("spawn")
     main()
