4f278a2d61ac5cc4f3d2de337c27606d1cf05b5b
saikrishna-1996 <saikrishnagv1996@gmail.com>
2022-09-08 14:53:55 -0400

    black format

--- environments/isaac_adapter.py
+++ environments/isaac_adapter.py
@@ -37,7 +37,7 @@ class Environment:
     def __init__(self, cfg):
         print("cfg = ", cfg)
         self.gym_env_name = cfg.env_name
-        print("self.gym_env_name = ",  self.gym_env_name)
+        print("self.gym_env_name = ", self.gym_env_name)
 
         # gym_env = gym.make(self.gym_env_name)
         gym_env = isaacgymenvs.make(
