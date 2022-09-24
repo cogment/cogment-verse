5b14a2469e2db78e8d26561efefc8650c7d05a9f
saikrishnagv_1996 <saikrishnagv1996@gmail.com>
2022-09-24 15:54:37 -0400

    environment service starting

--- environments/robosuite_adapter.py
+++ environments/robosuite_adapter.py
@@ -33,64 +33,169 @@ from cogment_verse.constants import PLAYER_ACTOR_CLASS, TEACHER_ACTOR_CLASS
 # configure pygame to use a dummy video server to be able to render headlessly
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 
+import os
+import shutil
+
+import hydra
+from omegaconf import OmegaConf
+from omegaconf import DictConfig
+from hydra.core.hydra_config import HydraConfig
+
+
+def build_feature_obs_robosuite_env(cfg):
+    # Acknowledgement: https://github.com/melfm/simrealjaco
+    import robosuite
+    from robosuite.wrappers.gym_wrapper import GymWrapper
+    robo_cfg = cfg.robosuite.params
+
+    if 'controller_config_file' in robo_cfg.keys():
+        control_cfg_file = robo_cfg['controller_config_file']
+        assert control_cfg_file.endswith('.json')
+        print('json file required')
+        control_cfg_filepath = os.path.join(
+            os.path.split(robosuite.__file__)[0], 'controllers', 'config',
+            control_cfg_file)
+        print('loading controller from', control_cfg_filepath)
+        controller_configs = robosuite.load_controller_config(
+            custom_fpath=control_cfg_filepath)
+    else:
+        controller_configs = robosuite.load_controller_config(
+            default_controller=robo_cfg['controller_name'])
+    options = {}
+
+    # Keep a copy of the controller config
+    robo_path = os.path.dirname(robosuite.__file__)
+    config_path = robo_path + '/controllers/config/'
+    config_file = config_path + robo_cfg.controller_config_file
+    shutil.copy(config_file, os.getcwd())
+
+    # not all configs are meant to go to robosuite
+    for key in robo_cfg.keys():
+        if key not in ['controller_name', 'controller_config_file']:
+            options[key] = robo_cfg[key]
+
+    # Sanity check to make sure controller_name and controller_config_file
+    # are a match.
+    if robo_cfg['controller_name'] != controller_configs['type']:
+        raise ValueError('Controller spec mismatch!')
+
+    env = robosuite.make(
+        controller_configs=controller_configs,
+        use_camera_obs=False,
+        use_object_obs=True,
+        ignore_done=False,
+        hard_reset=True,
+        has_offscreen_renderer=True,
+        has_renderer=False,
+        reward_scale=1.0,
+        camera_names='frontview',
+        **options,
+    )
+
+    class CustomRobosuiteWrapper(GymWrapper):
+
+        def render(self, mode=''):
+            """ make robosuite compatible with sac video render code """
+            # camera_widths is 256
+            return self.env.sim.render(
+                camera_name=self.env.camera_names[0],
+                width=self.env.camera_widths[0],
+                height=self.env.camera_heights[0])[::-1, :]
+
+    env = CustomRobosuiteWrapper(env)
+    env.max_path_length = robo_cfg.horizon
+    env._max_episode_steps = robo_cfg.horizon
+    return env
+
+
+class EnvSampler():
+    # Acknowledgement: https://github.com/melfm/simrealjaco
+    """Env Class used for sampling environments with different
+        simulation parameters.
+    """
+
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.env_name = self.cfg.env_name
+        # self.env_type = self.cfg.env_type
+
+    def sample_env(self):
+        env = build_feature_obs_robosuite_env(self.cfg)
+        env.reset()
+        env._max_episode_steps = env.max_path_length
+        return env
+
 
 class Environment:
     def __init__(self, cfg):
         self.gym_env_name = cfg.env_name
-        options = {}
-        options["env_name"] = choose_environment() #ToDo: replace with gym_env_name
-        if "TwoArm" in options["env_name"]:
-            # Choose env config and add it to options
-            options["env_configuration"] = choose_multi_arm_config()
-
-            # If chosen configuration was bimanual, the corresponding robot must be Baxter. Else, have user choose robots
-            if options["env_configuration"] == "bimanual":
-                options["robots"] = "Baxter"
-            else:
-                options["robots"] = []
-
-                # Have user choose two robots
-                print("A multiple single-arm configuration was chosen.\n")
-
-                for i in range(2):
-                    print("Please choose Robot {}...\n".format(i))
-                    options["robots"].append(choose_robots(exclude_bimanual=True))
-
-            # Else, we simply choose a single (single-armed) robot to instantiate in the environment
-        else:
-            options["robots"] = choose_robots(exclude_bimanual=True)
-
-            # Choose controller
-        controller_name = choose_controller()
-
-        # Load the desired controller
-        options["controller_configs"] = load_controller_config(default_controller=controller_name)
-
-        # Help message to user
-        print()
-        print('Press "H" to show the viewer control panel.')
-
-        # initialize the task
-        env = suite.make(
-            **options,
-            has_renderer=True,
-            has_offscreen_renderer=False,
-            ignore_done=True,
-            use_camera_obs=False,
-            control_freq=20,
-        )
-        env.reset()
-        env.viewer.set_camera(camera_id=0)
-        print(" ****** env ****** ")
-        print(env)
-        print(" ****** env ****** ")
+        print("cfg = ", cfg)
+        # print("aftrer config")
+        # print()
+        # self.work_dir = os.getcwd()
+        # print(f'workspace: {self.work_dir}')
+        # hydra_cfg = HydraConfig.get()
+        # config_file = self.work_dir.split('runs')[0] + 'config/' \
+        #               + hydra_cfg.job.config_name + '.yaml'
+        # shutil.copy(config_file, self.work_dir)
+        #
+        # self.cfg = cfg
+        self.env = EnvSampler(cfg).sample_env()
+        print(" wohooo ")
+        # options = {}
+        # options["env_name"] = choose_environment() #ToDo: replace with gym_env_name
+        # if "TwoArm" in options["env_name"]:
+        #     # Choose env config and add it to options
+        #     options["env_configuration"] = choose_multi_arm_config()
+        #
+        #     # If chosen configuration was bimanual, the corresponding robot must be Baxter. Else, have user choose robots
+        #     if options["env_configuration"] == "bimanual":
+        #         options["robots"] = "Baxter"
+        #     else:
+        #         options["robots"] = []
+        #
+        #         # Have user choose two robots
+        #         print("A multiple single-arm configuration was chosen.\n")
+        #
+        #         for i in range(2):
+        #             print("Please choose Robot {}...\n".format(i))
+        #             options["robots"].append(choose_robots(exclude_bimanual=True))
+        #
+        #     # Else, we simply choose a single (single-armed) robot to instantiate in the environment
+        # else:
+        #     options["robots"] = choose_robots(exclude_bimanual=True)
+        #
+        #     # Choose controller
+        # controller_name = choose_controller()
+        #
+        # # Load the desired controller
+        # options["controller_configs"] = load_controller_config(default_controller=controller_name)
+        #
+        # # Help message to user
+        # print()
+        # print('Press "H" to show the viewer control panel.')
+        #
+        # # initialize the task
+        # env = suite.make(
+        #     **options,
+        #     has_renderer=True,
+        #     has_offscreen_renderer=False,
+        #     ignore_done=True,
+        #     use_camera_obs=False,
+        #     control_freq=20,
+        # )
+        # env.reset()
+        # env.viewer.set_camera(camera_id=0)
+        # print(" ****** env ****** ")
+        # print(env)
+        # print(" ****** env ****** ")
 
         # gym_env = gym.make(self.gym_env_name)
         self.env_specs = EnvironmentSpecs(
             num_players=1,
             turn_based=False,
-            observation_space=space_from_gym_space(env.observation_space),
-            action_space=space_from_gym_space(env.action_space),
+            observation_space=space_from_gym_space(self.env.observation_space),
+            action_space=space_from_gym_space(self.env.action_space),
         )
 
     def get_implementation_name(self):
