31203863be07fa305dff1471107c214c37c7256f
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2023-03-10 18:34:36 -0500

    Fix license

--- cogment_verse/specs/__init__.py
+++ cogment_verse/specs/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import cog_settings  # pylint: disable=import-error
 from data_pb2 import AgentConfig, EnvironmentConfig  # pylint: disable=import-error
 
