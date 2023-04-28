999a7bd41334f1fe3101b3fac260023374b176f5
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2023-04-28 18:11:44 -0400

    chore: remove jupyter notebook

--- build_and_push_docker.sh
+++ build_and_push_docker.sh
@@ -16,7 +16,7 @@ account=$(aws sts get-caller-identity --query Account --output text)
 
 # Get the region defined in the current configuration (default to us-west-2 if none defined)
 region=$(aws configure get region)
-region=${region:-us-west-2}
+# region=${region:-us-west-2}
 fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
 
 # If the repository doesn't exist in ECR, create it.
--- run_both.py
+++ run_both.py
@@ -1,11 +1,7 @@
-import json
-import multiprocessing
-import os
-import subprocess
 
-# PREFIX = "./"
-PREFIX = "/opt/ml/"  # Sagemaker's required prefix
-PARAM_PATH = os.path.join(PREFIX, "input/config/")
+import multiprocessing
+import subprocess
+from sagemaker_containers import training_job_utils
 
 
 def run_script(module_name, *args):
@@ -13,11 +9,8 @@ def run_script(module_name, *args):
 
 
 def main():
-    PARAM_PATH = os.path.join(PREFIX, "input/config/")
-    hyper_params_path = PARAM_PATH + "hyperparameters.json"
-    with open(hyper_params_path, "r", encoding="utf-8") as params:
-        hyper_params = json.load(params)
-    main_args = str(hyper_params["main_args"])
+    hyperparameters = training_job_utils.get_hyperparameters()
+    main_args = str(hyperparameters["main_args"])
     service_process = multiprocessing.Process(target=run_script, args=("simple_mlflow",))
     client_process = multiprocessing.Process(
         target=run_script,
