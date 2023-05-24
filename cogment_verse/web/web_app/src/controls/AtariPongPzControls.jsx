37dc2639ed8249d8620cf99cf176ad5e3af229b1
Clodéric Mars <cloderic@ai-r.com>
2023-05-24 14:10:29 -0400

    Update license

--- cogment_verse/web/web_app/src/controls/AtariPongPzControls.jsx
+++ cogment_verse/web/web_app/src/controls/AtariPongPzControls.jsx
@@ -1,4 +1,4 @@
-// Copyright 2021 AI Redefined Inc. <dev+cogment@ai-r.com>
+// Copyright 2023 AI Redefined Inc. <dev+cogment@ai-r.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
