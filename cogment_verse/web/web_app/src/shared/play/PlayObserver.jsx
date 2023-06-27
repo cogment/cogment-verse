a2d6961584d4cf094a7af57deedf5eae80fc7a22
wduguay-air <william@ai-r.com>
2023-06-27 13:57:07 -0400

    fix

--- cogment_verse/web/web_app/src/shared/play/PlayObserver.jsx
+++ cogment_verse/web/web_app/src/shared/play/PlayObserver.jsx
@@ -12,10 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useDocumentKeypressListener, useRealTimeUpdate } from "../hooks";
 import { TEACHER_NOOP_ACTION } from "../utils/spaceSerialization";
 import { Button, FpsCounter, KeyboardControlList, SimplePlay } from "../components";
+
 
 const TURN_DURATION_SECS = 1;
 
